29bf350ad8774d2b17f686db810111d0bf837464
juniorecheverri <juniorecheverri9@gmail.com>
2022-09-26 20:24:31 -0500

    Final final no da más

--- venv/controlador/ctrlAutomata.py
+++ venv/controlador/ctrlAutomata.py
@@ -14,8 +14,8 @@ class Automata():
         for acepta in aceptacion:
             for nuevo in estados:
                 print(acepta)
-                if (acepta == nuevo):
-                    posEstado = nuevos_aceptacion.index(acepta)
+                if (acepta == nuevo): #Si el estado está dentro de la lista de estados de aceptacion
+                    posEstado = nuevos_aceptacion.index(acepta) #guarda la posicion del estado
                     nuevos_aceptacion.pop(posEstado)
 
         automata["F"] = nuevos_aceptacion
@@ -23,16 +23,6 @@ class Automata():
         print(automata)
         
         return automata
-
-    def reversoAutomata(self, automata):
-        print(automata)
-        reverso = {}
-
-        if len(automata["F"])==1:
-            reverso = self.invertirAutomata(automata)
-            return reverso
-
-        return reverso
 
     def unionAutomatas(self, automata1, automata2):
         nuevoAutomata = {
@@ -62,11 +52,11 @@ class Automata():
                             if finales1 == estadosPrimero or finales2 == estadosSegundo:
                                 nuevoAutomata["F"].append(estadosPrimero + estadosSegundo)
         #----------------------------------------
-        for estadosP in automata1["Q"]:  # ciclos para crear la combinacion de estados
+        for estadosP in automata1["Q"]:  # ciclos para crear la combinacion de transiciones
             for estadosS in automata2["Q"]:
-                for transicion2 in automata2["transiciones"]:  # verifica las transiciones del primer automata
+                for transicion2 in automata2["transiciones"]:  # verifica las transiciones del segundo automata
                     for transicion1 in automata1["transiciones"]:  # verifica las transiciones del primer automata
-                        if transicion1[0] == estadosP and transicion2[0] == estadosS and transicion1[1] == transicion2[1]:  # con el mismo nombre pero inveritido
+                        if transicion1[0] == estadosP and transicion2[0] == estadosS and transicion1[1] == transicion2[1]:  # verifica si hay una ruta de la cada estado con el mismo alfabeto
                             nuevoAutomata["transiciones"].append([estadosP + estadosS, transicion1[1], transicion1[2] + transicion2[2]])
 
         print(nuevoAutomata)
@@ -114,16 +104,16 @@ class Automata():
         print(automata)
         reverso = {}
 
-        if len(automata["F"]) == 1:
+        if len(automata["F"]) == 1: #si el automata solo tiene un estado de aceptacion
             reverso = self.invertirAutomata(automata)
             return reverso
         else:
-            automata["Q"].append("auxiliar")
+            automata["Q"].append("auxiliar") #creamos el estado auxiliar
             for estado in automata["Q"]:
                 for aceptacion in automata["F"]:
-                    if estado == aceptacion:
+                    if estado == aceptacion: #si el estado es de aceptacion
                         automata["transiciones"].append([estado, "lamda", "auxiliar"])
-            automata["F"] = ["auxiliar"]
+            automata["F"] = ["auxiliar"] #reescribo la lista de estados de aceptacion
             reverso = self.invertirAutomata(automata)
             return reverso
         return reverso
@@ -139,17 +129,41 @@ class Automata():
         reverso["Q"] = automata["Q"]
         reverso["alfabeto"] = automata["alfabeto"]
 
-        for transicion in automata["transiciones"]:
+        for transicion in automata["transiciones"]: #ciclo para invertir las transiciones
             reverso["transiciones"].append([transicion[2], transicion[1], transicion[0]])
 
         copiaAceptacion = automata["F"]
         copiaInicial = automata["Q0"]
 
-        reverso["Q0"] = copiaAceptacion[0]
-        reverso["F"].append(copiaInicial)
+        reverso["Q0"] = copiaAceptacion[0] #pego el estado aceptacion del automata original en el estado inicial del nuevo automata
+        reverso["F"].append(copiaInicial) #pego el estado inicial del automata original en el estado de aceptacion del nuevo automata
 
         print("Revirtiendo el automata")
         time.sleep(1)
         print(reverso)
 
         return reverso
+
+    def verificarInalcanzable(self, automata):
+        encontrado = False
+
+        nuevo = copy(automata)
+
+        for estado in automata["Q"]:
+            encontrado = False
+            for transicion in automata["transiciones"]:
+
+                if transicion[2] == estado and transicion[0] != estado and estado != automata["Q0"]:
+                    encontrado = True
+                    break
+            if not encontrado:
+                posEstado = nuevo["Q"].index(estado)
+                nuevo["Q"].pop(posEstado)
+
+                for transicion1 in automata["transiciones"]:
+                    if transicion1[0] == estado:
+                        posTransicion = nuevo["transiciones"].index(transicion1)
+                        nuevo["transiciones"].pop(posTransicion)
+        print("_______________________")
+        print(nuevo)
+        return nuevo
